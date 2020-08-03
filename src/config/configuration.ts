613af34a8458bbaa6122cfe64a7fed0d6f13783b
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2020-08-03 10:57:09 +0300

    fixed config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,9 @@
-const config = require('./config');
+import * as config from './config.json';
 export default () => ({
-    // port: parseInt(process.env.PORT, 10) || 3000,
-    // database: {
-    //     host: process.env.DATABASE_HOST,
-    //     port: parseInt(process.env.DATABASE_PORT, 10) || 5432
-    // },
-    ...config
+	// port: parseInt(process.env.PORT, 10) || 3000,
+	// database: {
+	//     host: process.env.DATABASE_HOST,
+	//     port: parseInt(process.env.DATABASE_PORT, 10) || 5432
+	// },
+	...config
 });

2c30bf9b31581947819b9026d30af5d2133c7972
Yona Ben-Reuven <yona@carmel6000.amitnet.org>
2021-02-07 12:20:02 +0200

    devdependencies

--- base/src/common/loginErrorCodes.ts
+++ base/src/common/loginErrorCodes.ts
@@ -1,5 +1,14 @@
+interface ErrorDictonery {
+	[name: string]: {
+		code: number;
+		message: string;
+		key: string;
+	};
+}
 
-export const LoginErrorCodes: ErrorDictonery = {
+const createLoginErrorCodes = <T extends ErrorDictonery>(errorCodes: T): T => errorCodes;
+
+export const LoginErrorCodes = createLoginErrorCodes({
 	PassDosentMatch: { key: "PassDosentMatch", code: 1, message: "Wrong password" },
 	NoUsername: { key: "NoUsername", code: 2, message: "User doesn't exist" },
 	EmailNotVerified: { key: "EmailNotVerified", code: 3, message: "Email must be verified" },
@@ -10,8 +19,4 @@ export const LoginErrorCodes: ErrorDictonery = {
 	MaxAttempts: { key: "MaxAttempts", code: 8, message: "Max attempts" },
 	CodeHasExpired: { key: "CodeHasExpired", code: 9, message: "Code has expired" },
 	IncorrectCode: { key: "IncorrectCode", code: 10, message: "Incorrect code" }
-}
-
-interface ErrorDictonery {
-	[name: string]: { code: number, message: string, key: string }
-}
+});
